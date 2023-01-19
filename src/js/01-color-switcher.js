666662218da6454a7f3d9f701078ec76f095ffa9
Ira Akulova <ira.akulova2019@gmail.com>
2023-01-19 11:27:19 +0200

    01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+let timerId = null;
+  
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', '');
+  refs.stopBtn.removeAttribute('disabled', '');
+
+  switchBodyColor();
+  timerId = setInterval(switchBodyColor, 1000);
+ };
+
+function onStopBtnClick() {
+  refs.stopBtn.setAttribute('disabled', '');
+  refs.startBtn.removeAttribute('disabled', '');
+
+  clearInterval(timerId);
+ };
+
+function switchBodyColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
