534808e24ba4c7e58f15ee76fc2087e84f88cf6c
Ira Akulova <ira.akulova2019@gmail.com>
2023-01-24 20:04:37 +0200

    in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  // delayInp: document.querySelector('[name="delay"]'),
+  // stepInp: document.querySelector('[name="step"]'),
+  // amountInp: document.querySelector('[name="amount"]'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+  e.preventDefault();
+  const delay = e.target.delay.value;
+  const step = e.target.step.value;
+  const amount = e.target.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newDelay = Number(step) * Number(i) + Number(delay);
+    setTimeout(createPromise, newDelay)}};
+
+  function createPromise(position, delay) {
+    
+    return new Promise(() => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        } else {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }
+      }, 0)
+    })
+};
+
+
+
